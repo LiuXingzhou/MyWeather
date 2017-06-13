2a8005fb618ce29b97e6e8206dee7033b69bd820
Qingsu <xingzhou.liu@foxmail.com>
2017-06-13 11:32:32 +0800

    test

---
+++ app/src/main/java/com/islxz/myweather/fragment/WeatherFragment.java
@@ -0,0 +1,25 @@
+package com.islxz.myweather.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.islxz.myweather.R;
+
+/**
+ * Created by Qingsu on 2017/6/13.
+ */
+
+public class WeatherFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle
+            savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_weather, null);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
